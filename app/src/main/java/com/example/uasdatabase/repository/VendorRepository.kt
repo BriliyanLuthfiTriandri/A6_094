4e2948a16c4b1bc75a75819c156aee3602a9b792
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-26 05:39:09 +0700

    Menambahkan class NetworkVendorRepository untuk operasi CRUD pada vendor menggunakan API

--- app/src/main/java/com/example/uasdatabase/repository/VendorRepository.kt
+++ app/src/main/java/com/example/uasdatabase/repository/VendorRepository.kt
@@ -19,3 +19,40 @@ interface VendorRepository {
     suspend fun getVendorById(idVendor: Int): VendorResponseDetail
 }
 
+class NetworkVendorRepository(
+    private val vendorApiService: VendorService
+) : VendorRepository {
+
+    override suspend fun insertVendor(vendor: Vendor) {
+        vendorApiService.insertVendor(vendor)
+    }
+
+    override suspend fun updateVendor(idVendor: Int, vendor: Vendor) {
+        vendorApiService.updateVendor(idVendor, vendor)
+    }
+
+    override suspend fun deleteVendor(idVendor: Int) {
+        try {
+            val response = vendorApiService.deleteVendor(idVendor)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete Vendor. HTTP Status code: " +
+                            "${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getVendor(): VendorResponse {
+        return vendorApiService.getVendor()
+    }
+
+    override suspend fun getVendorById(idVendor: Int): VendorResponseDetail {
+        return vendorApiService.getVendorById(idVendor)
+    }
+}
