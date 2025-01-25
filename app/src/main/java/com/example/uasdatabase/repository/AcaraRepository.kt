7b331af3e1adc88f66424c1b8d225c290209c522
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-26 05:43:24 +0700

    Menambahkan kelas NetworkAcaraRepository untuk operasi CRUD pada acara menggunakan API

--- app/src/main/java/com/example/uasdatabase/repository/AcaraRepository.kt
+++ app/src/main/java/com/example/uasdatabase/repository/AcaraRepository.kt
@@ -19,3 +19,38 @@ interface AcaraRepository {
     suspend fun getAcaraById(id_acara: Int): AcaraResponseDetail
 }
 
+class NetworkAcaraRepository(
+    private val acaraApiService: AcaraService
+) : AcaraRepository {
+
+    override suspend fun insertAcara(acara: Acara) {
+        acaraApiService.insertAcara(acara)
+    }
+
+    override suspend fun updateAcara(id_acara: Int, acara: Acara) {
+        acaraApiService.updateAcara(id_acara, acara)
+    }
+
+    override suspend fun deleteAcara(id_acara: Int) {
+        try {
+            val response = acaraApiService.deleteAcara(id_acara)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete Acara. HTTP Status code: ${response.code()}"
+                )
+            } else {
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getAcara(): AcaraResponse {
+        return acaraApiService.getAcara()
+    }
+
+    override suspend fun getAcaraById(id_acara: Int): AcaraResponseDetail {
+        return acaraApiService.getAcaraById(id_acara)
+    }
+}
