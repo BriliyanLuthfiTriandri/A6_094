d606de2e1498df4f9efed807910211bf97bd02dd
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-26 05:42:05 +0700

    Menambahkan kelas NetworkKlienRepository untuk operasi CRUD pada klien menggunakan API

--- app/src/main/java/com/example/uasdatabase/repository/KlienRepository.kt
+++ app/src/main/java/com/example/uasdatabase/repository/KlienRepository.kt
@@ -19,3 +19,38 @@ interface KlienRepository {
     suspend fun getKlienById(id_klien: Int): KlienResponseDetail
 }
 
+class NetworkKlienRepository(
+    private val klienApiService: KlienService
+) : KlienRepository {
+
+    override suspend fun insertKlien(klien: Klien) {
+        klienApiService.insertKlien(klien)
+    }
+
+    override suspend fun updateKlien(id_klien: Int, klien: Klien) {
+        klienApiService.updateKlien(id_klien, klien)
+    }
+
+    override suspend fun deleteKlien(id_klien: Int) {
+        try {
+            val response = klienApiService.deleteKlien(id_klien)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete Klien. HTTP Status code: ${response.code()}"
+                )
+            } else {
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getKlien(): KlienResponse {
+        return klienApiService.getKlien()
+    }
+
+    override suspend fun getKlienById(id_klien: Int): KlienResponseDetail {
+        return klienApiService.getKlienById(id_klien)
+    }
+}
