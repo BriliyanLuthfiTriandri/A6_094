0703a4a021186dfb85262200444d490aa85a2e52
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-26 05:40:54 +0700

    Menambahkan kelas NetworkLokasiRepository untuk operasi CRUD pada lokasi menggunakan API

--- app/src/main/java/com/example/uasdatabase/repository/LokasiRepository.kt
+++ app/src/main/java/com/example/uasdatabase/repository/LokasiRepository.kt
@@ -19,4 +19,40 @@ interface LokasiRepository {
     suspend fun getLokasiById(id_lokasi: Int): LokasiResponseDetail
 }
 
+class NetworkLokasiRepository(
+    private val lokasiApiService: LokasiService
+) : LokasiRepository {
 
+    override suspend fun insertLokasi(lokasi: Lokasi) {
+        lokasiApiService.insertLokasi(lokasi)
+    }
+
+    override suspend fun updateLokasi(id_lokasi: Int, lokasi: Lokasi) {
+        lokasiApiService.updateLokasi(id_lokasi, lokasi)
+    }
+
+    override suspend fun deleteLokasi(id_lokasi: Int) {
+        try {
+            val response = lokasiApiService.deleteLokasi(id_lokasi)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete Vendor. HTTP Status code: " +
+                            "${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getLokasi(): LokasiResponse {
+        return lokasiApiService.getLokasi()
+    }
+
+    override suspend fun getLokasiById(id_lokasi: Int): LokasiResponseDetail {
+        return lokasiApiService.getLokasiById(id_lokasi)
+    }
+}
