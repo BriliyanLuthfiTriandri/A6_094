4c48b97a1bb8b4d930e028381a0e9e0633ba0e73
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-27 20:20:05 +0700

    Menambahkan fungsi untuk memperbarui data Lokasi

---
+++ app/src/main/java/com/example/uasdatabase/ui/view/lokasi/UpdateLokasiView.kt
@@ -0,0 +1,65 @@
+package com.example.uasdatabase.ui.view.lokasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uasdatabase.ui.customwidget.CostumeTopAppBar
+import com.example.uasdatabase.ui.navigation.AlamatNavigasi
+import com.example.uasdatabase.ui.viewmodel.PenyediaViewModel
+import com.example.uasdatabase.ui.viewmodel.lokasi.UpdateLokasiViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateLokasiScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate: () -> Unit,
+    viewModel: UpdateLokasiViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateLokasi.titleRes,
+                canNavigateBack = true,
+                showMenu = false,
+                showRefresh = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ) { padding ->
+        EntryLokasiBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.updateUiState,
+            onLokasiValueChange = { event ->
+                viewModel.updateInsertLokasiState(event)
+            },
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateLokasi()
+                    delay(600)
+                    withContext(Dispatchers.Main) {
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
+
