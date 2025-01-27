5fb50d77825f136c61eda84c8b935b09cb75a595
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-27 16:50:04 +0700

    Menambahkan fungsi untuk mengonversi objek Acara menjadi InsertAcaraUiEvent

---
+++ app/src/main/java/com/example/uasdatabase/ui/viewmodel/acara/InsertAcaraViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.uasdatabase.ui.viewmodel.acara
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uasdatabase.model.Acara
+import com.example.uasdatabase.repository.AcaraRepository
+import kotlinx.coroutines.launch
+
+
+
+fun Acara.toInsertAcaraUiEvent(): InsertAcaraUiEvent = InsertAcaraUiEvent(
+    id_acara = id_acara,
+    id_lokasi = id_lokasi,
+    id_klien = id_klien,
+    nama_acara = nama_acara,
+    deskripsi_acara = deskripsi_acara,
+    tanggal_mulai = tanggal_mulai,
+    tanggal_berakhir = tanggal_berakhir,
+    status_acara = status_acara,
+
+)
