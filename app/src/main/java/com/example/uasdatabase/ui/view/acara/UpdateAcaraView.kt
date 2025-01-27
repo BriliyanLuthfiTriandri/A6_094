83c2f1357a761615c55ef148530195b8e0cfb5bf
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-28 00:31:08 +0700

    Menambahkan fungsi UpdateAcaraScreen untuk mengupdate data acara

---
+++ app/src/main/java/com/example/uasdatabase/ui/view/acara/UpdateAcaraView.kt
@@ -0,0 +1,65 @@
+package com.example.uasdatabase.ui.view.acara
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uasdatabase.ui.customwidget.CostumeTopAppBar
+import com.example.uasdatabase.ui.navigation.AlamatNavigasi
+import com.example.uasdatabase.ui.viewmodel.PenyediaViewModel
+import com.example.uasdatabase.ui.viewmodel.acara.UpdateAcaraViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateAcaraScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate: () -> Unit,
+    viewModel: UpdateAcaraViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateAcara.titleRes,
+                canNavigateBack = true,
+                showMenu = false,
+                showRefresh = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ) { padding ->
+        EntryAcaraBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.updateUiState,
+            onAcaraValueChange = {
+                event ->
+                viewModel.updateInsertAcaraState(event)
+            },
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateAcara()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
+
